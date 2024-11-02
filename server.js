fb7521189b9a968b1c050caa2e879f4141035690
Sridhar-Shankar <157667846+Sridhar-Shankar@users.noreply.github.com>
2024-11-02 20:13:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require("cors")
 const dotenv = require("dotenv")
 dotenv.config()
 app.use(cors({
-    origin: 'https://task-management-frontend-ruddy.vercel.app'  
+   "*"  
 }));
 
 app.use(bodyParser.urlencoded({extended:false}))
